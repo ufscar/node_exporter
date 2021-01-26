5cb4ccb3423d9d1b5d27d5920d005baa1798a5c0
afsmaira <andre.smaira@alumni.usp.br>
2021-01-26 17:56:53 -0300

    Testing CI

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -3,5 +3,6 @@ cd ..
 mkdir "${NE_NAME}-${NE_VERSION}"
 cp -a "${NE_NAME}/." "${NE_NAME}-${NE_VERSION}/"
 tar -czvf "${HOME}/rpmbuild/SOURCES/${targz}" "${NE_NAME}-${NE_VERSION}"
+
 cd "${NE_NAME}" || exit 1
 make
