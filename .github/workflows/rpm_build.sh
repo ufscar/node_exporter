e3d490a283766fcf565e551e173a8b400621507e
afsmaira <andre.smaira@alumni.usp.br>
2021-02-21 13:08:51 -0300

    Testing CI

--- .github/workflows/rpm_build.sh
+++ .github/workflows/rpm_build.sh
@@ -1,5 +1,3 @@
-orig=$(pwd)
-cd "${NE_NAME}" || exit 1
 cp "${NE_NAME}" "${HOME}/rpmbuild/BUILDROOT/${NE_NAME}"
 cp "lib/systemd/${NE_NAME}.service" ~/rpmbuild/SOURCES/
 cp "packages/rpm/${NE_NAME}.spec" ~/rpmbuild/SPECS/
@@ -8,4 +6,3 @@ rpmbuild -bb "${NE_NAME}.spec"
 f=$(ls "$HOME/rpmbuild/RPMS/x86_64/")
 cp "$HOME/rpmbuild/RPMS/x86_64/$f" "$orig/RPM/${NE_NAME}.rpm"
 rm -f "$HOME/rpmbuild/RPMS/x86_64/$f"
-cd "$orig" || exit 1
